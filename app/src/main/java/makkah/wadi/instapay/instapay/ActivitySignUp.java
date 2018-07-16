644e6c88cc21f9eb96e029155c57c460f328bbdb
unknown <saraalmakkawi@gmail.com>
2018-07-16 18:27:49 +0300

    first

---
+++ app/src/main/java/makkah/wadi/instapay/instapay/ActivitySignUp.java
@@ -0,0 +1,13 @@
+package makkah.wadi.instapay.instapay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
